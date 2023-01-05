0c2c321ed9a3c9b554e0aeaec86b51e5bb12d4dc
Guilherme Nascimento <guiguidefarias@hotmail.com>
2023-01-05 19:14:27 -0300

    Criação do projeto junto de seu dominio

---
+++ IWantApi/Domain/Products/Products.cs
@@ -0,0 +1,14 @@
+﻿namespace IWantApi.Domain.Products;
+
+public class Products
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
