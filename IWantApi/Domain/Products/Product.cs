96784547444a952ac2e0c4217122b573283b54f2
Guilherme Nascimento <guiguidefarias@hotmail.com>
2023-01-11 21:08:00 -0300

    Context do EF

--- IWantApi/Domain/Products/Product.cs
+++ IWantApi/Domain/Products/Product.cs
@@ -1,8 +1,9 @@
 ﻿namespace IWantApi.Domain.Products;
 
-public class Products : Entity
+public class Product : Entity
 {
     public string Name { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public string Description { get; set; }
     public bool HasStock { get; set; }
---
+++ IWantApi/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using IWantApi.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApi.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+              .Property(p => p.Description).HasMaxLength(255);
+        builder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+
+        builder.Entity<Category>()
+            .Property(p => p.Name).IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+    {
+        //todo string terá um máximo de caracteres de 100
+        configuration.Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
